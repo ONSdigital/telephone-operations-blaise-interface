82ce9309da9ac172d1a7093dfa974c485c975fdf
Lint Action <lint-action@samuelmeuli.com>
2020-10-27 14:28:50 +0000

    Fix code style issues with ESLint

--- Functions.js
+++ Functions.js
@@ -1,64 +1,64 @@
 function field_period_to_text(field_period) {
     let month_number = field_period.substr(5, 2);
-    let month = "Unknown"
+    let month = "Unknown";
 
     switch (month_number) {
     case "01":
-        month = "January"
+        month = "January";
         break;
     case "02":
-        month = "February"
+        month = "February";
         break;
     case "03":
-        month = "March"
+        month = "March";
         break;
     case "04":
-        month = "April"
+        month = "April";
         break;
     case "05":
-        month = "May"
+        month = "May";
         break;
     case "06":
-        month = "June"
+        month = "June";
         break;
     case "07":
-        month = "July"
+        month = "July";
         break;
     case "08":
-        month = "August"
+        month = "August";
         break;
     case "09":
-        month = "September"
+        month = "September";
         break;
     case "10":
-        month = "October"
+        month = "October";
         break;
     case "11":
-        month = "November"
+        month = "November";
         break;
     case "12":
-        month = "December"
+        month = "December";
         break;
     }
 
     let year_number = field_period.substr(3, 2);
-    let year = ""
+    let year = "";
 
     switch (year_number) {
     case "19":
-        year = "2019"
+        year = "2019";
         break;
     case "20":
-        year = "2020"
+        year = "2020";
         break;
     case "21":
-        year = "2021"
+        year = "2021";
         break;
     case "22":
-        year = "2021"
+        year = "2021";
         break;
     }
-    return month + " " + year
+    return month + " " + year;
 }
 
-module.exports = { field_period_to_text }
+module.exports = { field_period_to_text };
