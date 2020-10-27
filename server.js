93f88db2a8064b2c33981f37ef79b9c2a0979626
Matthew Palmer <matthewandrewpalmer@outlook.com>
2020-10-27 13:57:30 +0000

    Test auto fixing

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const server = express();
 const axios_instance = axios.create();
 axios_instance.defaults.timeout = 3000;
 
-if (process.env.NODE_ENV !== 'production') {
+
+if process.env.NODE_ENV !== 'production' {
     require('dotenv').config();
 }
 
