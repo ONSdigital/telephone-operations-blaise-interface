bac911e65a813812e125f6a1d1f4bd0423789fb8
Lint Action <lint-action@samuelmeuli.com>
2020-11-06 11:56:51 +0000

    Fixing up your untidy code 👇 with ESLint

--- server/Functions.js
+++ server/Functions.js
@@ -1,64 +1,64 @@
 function field_period_to_text(field_period) {
-    let month_number = field_period.substr(5, 2)
-    let month = "Unknown"
+    let month_number = field_period.substr(5, 2);
+    let month = "Unknown";
 
     switch (month_number) {
     case "01":
-        month = "January"
-        break
+        month = "January";
+        break;
     case "02":
-        month = "February"
-        break
-    case '03':
-        month = 'March'
-        break
-    case '04':
-        month = 'April'
-        break
-    case '05':
-        month = 'May'
-        break
-    case '06':
-        month = 'June'
-        break
-    case '07':
-        month = "July"
-        break
+        month = "February";
+        break;
+    case "03":
+        month = "March";
+        break;
+    case "04":
+        month = "April";
+        break;
+    case "05":
+        month = "May";
+        break;
+    case "06":
+        month = "June";
+        break;
+    case "07":
+        month = "July";
+        break;
     case "08":
-        month = "August"
-        break
+        month = "August";
+        break;
     case "09":
-        month = "September"
-        break
+        month = "September";
+        break;
     case "10":
-        month = "October"
-        break
+        month = "October";
+        break;
     case "11":
-        month = "November"
-        break
+        month = "November";
+        break;
     case "12":
-        month = "December"
-        break
+        month = "December";
+        break;
     }
 
-    let year_number = field_period.substr(3, 2)
-    let year = ""
+    let year_number = field_period.substr(3, 2);
+    let year = "";
 
     switch (year_number) {
     case "19":
-        year = "2019"
-        break
+        year = "2019";
+        break;
     case "20":
-        year = "2020"
-        break
+        year = "2020";
+        break;
     case "21":
-        year = "2021"
-        break
+        year = "2021";
+        break;
     case "22":
-        year = "2022"
-        break
+        year = "2022";
+        break;
     }
-    return month + " " + year
+    return month + " " + year;
 }
 
-module.exports = { field_period_to_text }
+module.exports = { field_period_to_text };
