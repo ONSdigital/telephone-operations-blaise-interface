f9e7795f156fb92747dd53e53f1bb78bceb87a9f
Matthew Palmer <matthewandrewpalmer@outlook.com>
2020-12-14 16:53:15 +0000

    Config.ts Tests

--- server/Config.ts
+++ server/Config.ts
@@ -11,17 +11,17 @@ export function getEnvironmentVariables(): EnvironmentVariables {
 
     if (BLAISE_API_URL === undefined) {
         console.error("BLAISE_API_URL environment variable has not been set");
-        BLAISE_API_URL = "";
+        BLAISE_API_URL = "ENV_VAR_NOT_SET";
     }
 
     if (VM_EXTERNAL_WEB_URL === undefined) {
         console.error("VM_EXTERNAL_WEB_URL environment variable has not been set");
-        VM_EXTERNAL_WEB_URL = "";
+        VM_EXTERNAL_WEB_URL = "ENV_VAR_NOT_SET";
     }
 
     if (VM_EXTERNAL_CLIENT_URL === undefined) {
         console.error("VM_EXTERNAL_WEB_URL environment variable has not been set");
-        VM_EXTERNAL_CLIENT_URL = "";
+        VM_EXTERNAL_CLIENT_URL = "ENV_VAR_NOT_SET";
     }
     return {VM_EXTERNAL_CLIENT_URL, VM_EXTERNAL_WEB_URL, BLAISE_API_URL, CATI_DASHBOARD_URL};
 }
